e68a72fc581bb2188b191d2f91cd4e743d86752a
Piyush Terkar <terkar.piyush3@gmail.com>
2022-09-06 22:35:08 +0530

    Initial Commit ("GANPATI BAPPA MORYA!!!")

---
+++ backend/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("We're are ONLINE!!");
+});
+
+app.listen(5000, () => {
+  console.log("Listening on Port 5000");
+});
