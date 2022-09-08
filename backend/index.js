ad66676d8fab7149f2d43ca98228d7c5e366c619
Piyush Terkar <terkar.piyush3@gmail.com>
2022-09-08 17:04:00 +0530

    implement upload files mechanism

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+const { upload } = require("./computationalUnit/fileupload");
+
+app.use(express.urlencoded());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.send("We're are ONLINE!!");
+});
+
+app.post("/users/upload", upload.single("file"), (req, res) => {
+  res.send("Uploaded");
 });
 
 app.listen(5000, () => {
