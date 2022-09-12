a0729c88fa7cd9abe6e89e1e88b7f23bf7c2625f
Kedar-Khedkar <70146608+Kedar-Khedkar@users.noreply.github.com>
2022-09-12 12:05:11 +0530

    Theme applied

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,15 +3,40 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import {ThemeProvider,createTheme} from '@mui/material'
+import CssBaseline from '@mui/material/CssBaseline';
+// import * as serviceWorker from './serviceWorker';
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const theme =createTheme({
+  palette:{
+    mode: 'dark',
+    primary: {
+      main: '#7827e7',
+    },
+    secondary: {
+      main: '#ea7ffc',
+    },
+    background: {
+      default: '#14142b',
+      paper: '#24263a',
+    },
+  },
+  typography:{
+    fontFamily: "'Poppins', sans-serif"
+  },
+})
 root.render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
+    <CssBaseline />
     <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
+// If you want to start measuring performance in your app, pa++++++++++ss a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// serviceWorker.register();
