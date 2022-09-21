09d4444a6ef13386ceaabcd5986ca22ff95f98d4
Kedar-Khedkar <70146608+Kedar-Khedkar@users.noreply.github.com>
2022-09-21 23:00:27 +0530

    minor fixes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,34 +3,34 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {ThemeProvider,createTheme} from '@mui/material'
-import CssBaseline from '@mui/material/CssBaseline';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 // import * as serviceWorker from './serviceWorker';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const theme =createTheme({
-  palette:{
-    mode: 'dark',
-    primary: {
-      main: '#7827e7',
-    },
-    secondary: {
-      main: '#ea7ffc',
-    },
-    background: {
-      default: '#14142b',
-      paper: '#24263a',
-    },
-  },
-  typography:{
-    fontFamily: "'Poppins', sans-serif"
-  },
-})
+// const theme =createTheme({
+//   palette:{
+//     mode: 'dark',
+//     primary: {
+//       main: '#7827e7',
+//     },
+//     secondary: {
+//       main: '#ea7ffc',
+//     },
+//     background: {
+//       default: '#14142b',
+//       paper: '#24263a',
+//     },
+//   },
+//   typography:{
+//     fontFamily: "'Poppins', sans-serif"
+//   },
+// })
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-    <CssBaseline />
+ 
+    
     <App />
-    </ThemeProvider>
+    
   </React.StrictMode>
 );
 
