0a3a2a438594da8f8f9c7a48217e549697daa8e9
Kedar-Khedkar <70146608+Kedar-Khedkar@users.noreply.github.com>
2022-09-22 00:37:37 +0530

    minor fixes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,31 @@
 import "./App.css";
+import './index.css'
 import "./bootstrap.scss";
 import { BrowserRouter, Route, Redirect, Routes } from "react-router-dom";
 import React, { useEffect, useState, Suspense } from "react";
 // import Form from "./component/Login & Registration/Login";
-import PasswordReset from "./component/Login & Registration/ResetPassword";
-import UserAdd from "./component/Login & Registration/AddUser";
+// import PasswordReset from "./component/Login & Registration/ResetPassword";
+// import UserAdd from "./component/Login & Registration/AddUser";
 // import DropzoneBox from './component/Drop zone/Dropzone'
 
 const LoginForm = React.lazy(() =>
   import("./component/Login & Registration/Login")
 );
+const PasswordReset = React.lazy(() =>
+  import("./component/Login & Registration/ResetPassword")
+);
+const UserAdd = React.lazy(() =>
+  import("./component/Login & Registration/AddUser")
+);
+// const DropzoneBox = React.lazy(() =>
+//   import("./component/Drop zone/Dropzone")
+// );
 function App() {
   return (
     <Suspense
     // fallback={ }
     >
-      <div>
+      <div className='App'>
         <BrowserRouter>
           <Routes>
 
