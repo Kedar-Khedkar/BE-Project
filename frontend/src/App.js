2dad41d7a1d5ebe365f07fcc98f9d80d1a873cbc
Kedar-Khedkar <70146608+Kedar-Khedkar@users.noreply.github.com>
2022-09-21 23:01:32 +0530

    routing | lazy loading implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,32 @@
-import './App.css';
-import Form from './component/Login & Registration/Login'
-import DropzoneBox from './component/Drop zone/Dropzone'
+import "./App.css";
+import "./bootstrap.scss";
+import { BrowserRouter, Route, Redirect, Routes } from "react-router-dom";
+import React, { useEffect, useState, Suspense } from "react";
+// import Form from "./component/Login & Registration/Login";
+import PasswordReset from "./component/Login & Registration/ResetPassword";
+import UserAdd from "./component/Login & Registration/AddUser";
+// import DropzoneBox from './component/Drop zone/Dropzone'
+
+const LoginForm = React.lazy(() =>
+  import("./component/Login & Registration/Login")
+);
 function App() {
   return (
-    <>
-    {/* <Form/> */}
-    <DropzoneBox/>
-          </>
+    <Suspense
+    // fallback={ }
+    >
+      <div>
+        <BrowserRouter>
+          <Routes>
+
+            <Route path="/login" element={<LoginForm />} />
+            <Route path="/" element={<LoginForm />} />
+            <Route path="/reset-password" element={<PasswordReset />} />
+            <Route path="/add-user" element={<UserAdd />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </Suspense>
   );
 }
 
