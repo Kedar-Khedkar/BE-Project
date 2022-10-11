a342aa7edc37b78c6aa9abb4dfc2788378c1f6b5
MitaliC21 <mitalichougule21@gmail.com>
2022-10-12 00:11:05 +0530

    joi

---
+++ backend/schemas.js
@@ -0,0 +1,31 @@
+const BaseJoi = require('joi');
+const sanitizeHtml = require('sanitize-html');
+
+const extension = (joi) => ({
+    type: 'string',
+    base: joi.string(),
+    messages: {
+        'string.escapeHTML': '{{#label}} must not include HTML!'
+    },
+    rules: {
+        escapeHTML: {
+            validate(value, helpers) {
+                const clean = sanitizeHtml(value, {
+                    allowedTags: [],
+                    allowedAttributes: {},
+                });
+                if (clean !== value) return helpers.error('string.escapeHTML', { value })
+                return clean;
+            }
+        }
+    }
+});
+
+const Joi = BaseJoi.extend(extension);
+modules.export.userUploadSchema = Joi.object({
+    user: Joi.object({
+        name: Joi.string().required().escapeHTML(),
+        email: Joi.email().required().escapeHTML(),
+        roll: Joi.number().required().escapeHTML()
+    }).required()
+})
