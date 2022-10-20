17b271de31032748b180acc496c70779b823fff2
Piyush Terkar <terkar.piyush3@gmail.com>
2022-10-20 20:20:18 +0530

    implement user authentication

--- backend/index.js
+++ backend/index.js
@@ -2,23 +2,25 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const morgan = require("morgan");
-const fs = require("fs");
-const cors = require("cors")
-// const session = require("express-session");
-const ExpressError = require("./utils/ExpressError");
-const catchAsync = require("./utils/catchAsync");
+const cors = require("cors");
+const session = require("express-session");
+const sessionStore = require("connect-session-sequelize")(session.Store);
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const flash = require("connect-flash");
+const bcrypt = require("bcrypt");
 
-const { upload } = require("./computationalUnit/fileupload");
-const { extractUsers } = require("./computationalUnit/extractExcel");
+const { User } = require("./models/user");
+const ExpressError = require("./utils/ExpressError");
 const { sequelize, Sequelize } = require("./utils/database");
+const userRoutes = require("./routes/user");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors())
+app.use(cors());
+app.use(morgan("dev"));
 
 app.use(express.static(path.join(__dirname, "public")));
-
-app.use(morgan("dev"));
 
 try {
   sequelize.authenticate();
@@ -26,44 +28,79 @@ try {
 } catch (error) {
   console.error("Unable to connect to the database:", error);
 }
+
+const store = new sessionStore({
+  db: sequelize,
+});
+
+const sessionConfig = {
+  secret: "Ganpati Bappa Morya!",
+  store: store,
+  resave: false,
+  proxy: true,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    // secure: true,     UNCOMMENT BEFORE DEPLOYMENT
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+};
+
+app.use(session(sessionConfig));
+app.use(flash());
 sequelize.sync({ force: true });
 
-const { validateStudent, validateFaculty } = require("./middleware");
-const { User } = require("./models/user");
-app.post(
-  "/users/studentRegister",
-  validateStudent,
-  catchAsync(async (req, res) => {
-    const { user } = req.body;
-    await User.create(user);
-    res.send("Student added successfully!");
-  })
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "email",
+    },
+    async function verify(username, password, done) {
+      const user = await User.findOne({ where: { email: username } });
+      if (user === null) {
+        return done(null, false, {
+          message: "User not found",
+        });
+      }
+      console.log(user.passHash);
+      bcrypt.compare(password, user.passHash, (err, res) => {
+        if (err) {
+          return done(err);
+        }
+        if (res === false) {
+          return done(null, false, {
+            message: "Incorrect username or password.",
+          });
+        } else {
+          return done(null, user);
+        }
+      });
+    }
+  )
 );
 
-app.post(
-  "/users/facultyRegister",
-  validateFaculty,
-  catchAsync(async (req, res) => {
-    const { user } = req.body;
-    await User.create(user);
-    res.send("Faculty added successfully!");
-  })
-);
+passport.serializeUser(function (user, cb) {
+  process.nextTick(function () {
+    cb(null, { id: user.id, username: user.fullname });
+  });
+});
 
-app.post(
-  "/users/upload",
-  upload.single("file"),
-  catchAsync(async (req, res) => {
-    const uploadPath = req.file.path;
-    const result = await extractUsers(uploadPath).then((result) => {
-      return result;
-    });
-    fs.unlink(uploadPath, (err) => {
-      if (err) next(err);
-    });
-    res.send(result);
-  })
-);
+passport.deserializeUser(function (user, cb) {
+  process.nextTick(function () {
+    return cb(null, user);
+  });
+});
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
+});
+
+app.use("/users", userRoutes);
 
 app.all("*", (req, res, next) => {
   next(new ExpressError("Page Not Found", 404));
