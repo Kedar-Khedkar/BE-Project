a1fc6f336360c9be7118177494ccc3d49d05d815
Saurabhpl2408 <saurabhpl14@gmail.com>
2022-10-22 14:03:45 +0530

    Table and list

---
+++ frontend/src/component/Table/List.js
@@ -0,0 +1,30 @@
+const Users=[
+    {
+        email:"deepak@gmail.com",
+        fullName:"Deepak",
+        errorMsg:"123"
+    },
+    {
+        email:"deepak@gmail.com",
+        fullName:"Deepak",
+        errorMsg:"123"
+    },
+    {
+        email:"deepak@gmail.com",
+        fullName:"Deepak",
+        errorMsg:"123"
+    },
+    {
+        email:"deepak@gmail.com",
+        fullName:"Deepak",
+        errorMsg:"123"
+    },
+    {
+        email:"deepak@gmail.com",
+        fullName:"Deepak",
+        errorMsg:"123"
+    },
+    
+    ]
+    export default Users;
+    
