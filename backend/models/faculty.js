78405057a1f467b63ab480cfbda292ad07a2dbd7
Piyush Terkar <terkar.piyush3@gmail.com>
2022-10-26 10:45:44 +0530

    Define faculty model

--- backend/models/faculty.js
+++ backend/models/faculty.js
@@ -1,9 +1,7 @@
 const { sequelize, Sequelize } = require("../utils/database");
 
-const Faculty = sequelize.define("Faculties", {
-  facultyId: {
-    type: Sequelize.STRING,
-  },
-});
+const Faculty = sequelize.define("Faculties", {});
+
+Faculty.removeAttribute("id");
 
 module.exports = { Faculty };
