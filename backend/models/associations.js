2e6f6c22fc888ebc67c1caca59e30c818be1c488
Piyush Terkar <terkar.piyush3@gmail.com>
2022-10-30 15:21:57 +0530

    add attendance associtations

--- backend/models/associations.js
+++ backend/models/associations.js
@@ -2,6 +2,7 @@ const { User } = require("./user");
 const { Faculty } = require("./faculty");
 const { Subject } = require("./subject");
 const { Student } = require("./student");
+const { Attendance } = require("./attendance");
 
 //ONE-TO-ONE USER-FACULTY
 User.hasOne(Faculty, { foreignKey: { name: "userId", allowNull: false } });
@@ -14,3 +15,10 @@ Student.belongsTo(User, { foreignKey: "userId" });
 //ONE-TO-MANY SUBJECT-FACULTY
 Subject.hasMany(Faculty, { foreignKey: { allowNull: false } });
 Faculty.belongsTo(Subject);
+
+//ONE-TO-MANY Subject-ATTENDANCE
+Subject.hasMany(Attendance);
+Attendance.belongsTo(Subject);
+
+Student.hasMany(Attendance);
+Attendance.belongsTo(Student);
