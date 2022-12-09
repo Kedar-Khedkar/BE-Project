03da39b1db8f780b3a77cd334171914e419833db
Kedar-Khedkar <70146608+Kedar-Khedkar@users.noreply.github.com>
2022-12-09 23:46:05 +0530

    attendance page

---
+++ frontend/src/component/Attendance/Attendance.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import SamplePage from './SamplePage'
+import TableAttendance from './Table'
+export default function Attendance() {
+  
+  return (
+    <>
+   <SamplePage/>
+    </>
+  )
+}
