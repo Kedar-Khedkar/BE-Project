93acbe0b79a42b42c8501fdd92aa3e44934194f2
Kedar-Khedkar <70146608+Kedar-Khedkar@users.noreply.github.com>
2022-12-16 13:13:17 +0530

    layout renamed and new file attendance created

---
+++ frontend/src/component/Attendance/Layout.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+// import SamplePage from './SamplePage'
+import TableAttendance from './Table'
+import userData from '../Sample/userData'
+export default function Attendance() {
+  
+  return (
+    <>
+   <TableAttendance data={userData}/>
+  
+    </>
+  )
+}
+
+
