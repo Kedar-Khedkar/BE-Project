dac40e6bbcc8235c02a4c5e2091b652e4e28ead6
MitaliC21 <mitalichougule21@gmail.com>
2023-03-01 19:25:51 +0530

    User profile form

--- frontend/src/component/User Account/NonStudentAccount.jsx
+++ frontend/src/component/User Account/NonStudentAccount.jsx
@@ -7,7 +7,13 @@ import {
   Text,
   Divider,
   Badge,
+  Card,
+  TextInput,
+  Button,
+  ActionIcon,
 } from "@mantine/core";
+import { useForm } from "@mantine/form";
+import { IconPencil } from "@tabler/icons-react";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
@@ -25,8 +31,15 @@ const useStyles = createStyles((theme) => ({
 }));
 
 export default function NonStudentAccount({ data }) {
+  const initialValues = {fullname: data.fullname, email: data.email, role: data.role}
+  const [isNotEdit, setIsNotEdit] = useState(true);
+  const form = useForm({initialValues})
   const { classes, theme } = useStyles();
   console.log("inside component", data);
+  const updateUser = (event, id) =>{
+    event.preventDefault()
+    axios.put(`http://localhost:5000/users/${id}`, {user: form.values}, {withCredentials: true}).then((res)=>{}).catch((res)=>{})
+  }
 
   return (
     <>
@@ -60,6 +73,16 @@ export default function NonStudentAccount({ data }) {
             {data.role}
           </Badge>
         </Center>
+        <Card>
+          <ActionIcon ml={900} onClick={()=>{setIsNotEdit(!isNotEdit)}}>
+            <IconPencil></IconPencil>
+          </ActionIcon>
+          <form onSubmit={(e)=>{updateUser(e, data.id)}}>
+            <TextInput disabled={isNotEdit} label="Fullname" defaultValue={data.fullname} withAsterisk {...form.getInputProps("fullname")} description="Your name in firstname<space>lastname format"/>
+            <TextInput disabled={isNotEdit}label="Email" defaultValue={data.email} withAsterisk {...form.getInputProps("email")} description="email associated with your account"/>
+            <Button mt={12} type="submit" disabled={isNotEdit}>Edit</Button>
+          </form>
+        </Card>
       </Container>
     </>
   );
