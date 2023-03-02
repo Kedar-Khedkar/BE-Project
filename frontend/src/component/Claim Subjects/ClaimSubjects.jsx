bc5f813d4d3fffe057db3af5b9cc0433dfd084f6
Piyush Terkar <terkar.piyush3@gmail.com>
2023-03-02 22:26:12 +0530

    FIX: claim empty list

--- frontend/src/component/Claim Subjects/ClaimSubjects.jsx
+++ frontend/src/component/Claim Subjects/ClaimSubjects.jsx
@@ -1,56 +1,67 @@
-import { Button, MultiSelect } from '@mantine/core';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import { showNotification } from '@mantine/notifications';
-import UnclaimSubjects from './UnclaimSubjects';
+import { Button, MultiSelect } from "@mantine/core";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { showNotification } from "@mantine/notifications";
+import UnclaimSubjects from "./UnclaimSubjects";
 
 export default function ClaimSubjects(props) {
- const [ClaimSubjects, setClaimSubjects] = useState([]);
- const [alreadyClaimed, setAlreadyClaimed] = useState([]);
+  const [ClaimSubjects, setClaimSubjects] = useState([]);
+  const [alreadyClaimed, setAlreadyClaimed] = useState([]);
 
- useEffect(()=>{
-    axios.get("http://localhost:5000/faculty/", {withCredentials:true})
-    .then((res)=>{
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/faculty/", { withCredentials: true })
+      .then((res) => {
         setAlreadyClaimed(res.data.objects);
-    })
- },[])
+      });
+  }, []);
 
- const handleSubmit = ()=>{
+  const handleSubmit = () => {
     console.log(ClaimSubjects);
-    axios.post("http://localhost:5000/faculty/claimSubjects", {subIds:ClaimSubjects}, {withCredentials:true})
-    .then((res)=>{
-            showNotification({
-              title: "Success!",
-              message: "Subject claimed successfully",
-              color: "teal",
-              disallowClose: false,
-            })
-            console.log(res);
-    }).catch(
-        function(err){
-            showNotification({
-              title: "Failed!",
-              message: "Something went wrong.",
-              color: "red",
-            })
-            console.log(err)
-          }
-    )
- }
+    axios
+      .post(
+        "http://localhost:5000/faculty/claimSubjects",
+        { subIds: ClaimSubjects },
+        { withCredentials: true }
+      )
+      .then((res) => {
+        showNotification({
+          title: "Success!",
+          message: "Subject claimed successfully",
+          color: "teal",
+          disallowClose: false,
+        });
+        console.log(res);
+      })
+      .catch(function (err) {
+        showNotification({
+          title: "Failed!",
+          message: "Something went wrong.",
+          color: "red",
+        });
+        console.log(err);
+      });
+  };
   return (
     <>
-    <h2>Claim Subjects</h2>
-    <MultiSelect error="Pick atleast one subject" onChange={setClaimSubjects}
-    data={props.data}
-  
-      label="Select the subjects to claim"
-      placeholder="Select the subjects"
-    />
-    <Button mt={5} onClick={handleSubmit}>Claim</Button>
+      <h2>Claim Subjects</h2>
+      <MultiSelect
+        // error="Pick atleast one subject"
+        onChange={setClaimSubjects}
+        data={props.data}
+        label="Select the subjects to claim"
+        placeholder="Select the subjects"
+      />
+      <Button
+        mt={5}
+        onClick={handleSubmit}
+        disabled={ClaimSubjects.length == 0}
+      >
+        Claim
+      </Button>
 
-    <h2>Unclaim Subjects</h2>
-    <UnclaimSubjects data={alreadyClaimed}></UnclaimSubjects>
+      <h2>Unclaim Subjects</h2>
+      <UnclaimSubjects data={alreadyClaimed}></UnclaimSubjects>
     </>
-
   );
 }
