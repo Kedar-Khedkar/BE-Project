d56b5a36e5e8141f08cbb1cc310f6fa3ba0dc6f8
Piyush Terkar <terkar.piyush3@gmail.com>
2023-03-02 19:15:44 +0530

    FIX: access issue for student statistics

--- backend/routes/attendance.js
+++ backend/routes/attendance.js
@@ -4,14 +4,18 @@ const catchAsync = require("../utils/catchAsync");
 const { isLoggedIn, isFacultyOrAdmin } = require("../middleware");
 const attendance = require("../controllers/attendance");
 
-router.route("/multiple").post(isLoggedIn, isFacultyOrAdmin, catchAsync(attendance.markMultiple));
+router
+  .route("/multiple")
+  .post(isLoggedIn, isFacultyOrAdmin, catchAsync(attendance.markMultiple));
 
 router
   .route("/")
   .post(isLoggedIn, isFacultyOrAdmin, catchAsync(attendance.readAttendance))
-  .post(isLoggedIn,isFacultyOrAdmin,catchAsync(attendance.markAttendance));
+  .post(isLoggedIn, isFacultyOrAdmin, catchAsync(attendance.markAttendance));
 
-router.route("/stats").post(isLoggedIn,isFacultyOrAdmin ,catchAsync(attendance.statistics));
-router.route("/stud-stats").get(isFacultyOrAdmin, isLoggedIn, catchAsync(attendance.studStats));
+router
+  .route("/stats")
+  .post(isLoggedIn, isFacultyOrAdmin, catchAsync(attendance.statistics));
+router.route("/stud-stats").get(isLoggedIn, catchAsync(attendance.studStats));
 
 module.exports = router;
