d9ac0c9bbb7f6e087949e055d90ff91f150e618e
Kedar-Khedkar <70146608+Kedar-Khedkar@users.noreply.github.com>
2023-03-03 23:48:29 +0530

    Add: background image

--- frontend/src/component/Dashboard/Dashboard.jsx
+++ frontend/src/component/Dashboard/Dashboard.jsx
@@ -1,19 +1,30 @@
 import React from "react";
-import { Container, SimpleGrid } from "@mantine/core";
+import { Container, SimpleGrid,createStyles,BackgroundImage } from "@mantine/core";
 import NavigationCard from "./NavigationCard";
 import user from "../../assets/Images/new-entries-animate.svg";
 import attendance from "../../assets/Images/confirmed-attendance-animate.svg";
 import subjects from "../../assets/Images/thesis-animate.svg";
 import StudentDash from "../Attendance/StudentDash";
 import secureLocalStorage from "react-secure-storage";
+import backgroundImage from "../../assets/Images/ttten.svg"
+
+const useStyles = createStyles((theme) => ({
+  background:{
+    backgroundImage:`${backgroundImage}`
+  }
+
+}))
 
 export default function Dashboard() {
+  const { classes } = useStyles();
   const { role } = JSON.parse(secureLocalStorage.getItem("user"));
   return (
     <>
       {/* <Header/> */}
+      <BackgroundImage src={backgroundImage}>
+      
       <Container >
-        <SimpleGrid cols={2} >
+        <SimpleGrid cols={2} breakpoints={[{ maxWidth: 980, cols: 1, spacing: "md" }]} >
           {role === "admin" && (
             <NavigationCard title={"User"} link={"/user-mgmt/1"} image={user} />
           )}
@@ -34,6 +45,8 @@ export default function Dashboard() {
         </SimpleGrid>
         {role === "student" && <StudentDash></StudentDash>}
       </Container>
+      
+    </BackgroundImage>
     </>
   );
 }
