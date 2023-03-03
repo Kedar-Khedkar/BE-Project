85633e60331736dec2f2c098f585612015404ced
Kedar-Khedkar <70146608+Kedar-Khedkar@users.noreply.github.com>
2023-03-03 23:06:04 +0530

    Add: responsiveness to the component

--- frontend/src/component/Attendance/AttendanceDash.jsx
+++ frontend/src/component/Attendance/AttendanceDash.jsx
@@ -1,4 +1,4 @@
-import { Divider, Grid } from "@mantine/core";
+import { Divider, SimpleGrid } from "@mantine/core";
 import React from "react";
 import AnimatedProgressBarCircle from "../Charts/AnimatedProgressBarCircle";
 import AreaChart from "../Charts/AreaChart";
@@ -9,21 +9,22 @@ export default function AttendanceDash({ data }) {
       {data && (
         <>
           <h1>Departmentwise Distribution</h1>
-          <Divider></Divider>
-          <Grid mt={12}>
-            <Grid.Col span={4}>
+          <Divider />
+          <SimpleGrid cols={2} breakpoints={[{ maxWidth: 980, cols: 1, spacing: "md" }]} >
+            <div>
               <AnimatedProgressBarCircle
                 data={data.deptAvg[0].deptAvg}
                 title={"Average Department Attendance"}
-              ></AnimatedProgressBarCircle>
-            </Grid.Col>
-            <Grid.Col span={8}>
+              />
+            </div>
+
+            <div>
               <AreaChart
                 title={"Daily Average Attendance"}
                 data={data.dailyAvg}
-              ></AreaChart>
-            </Grid.Col>
-          </Grid>
+              />
+            </div>
+          </SimpleGrid>
         </>
       )}
     </>
