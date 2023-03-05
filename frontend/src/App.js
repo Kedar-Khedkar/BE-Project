1d95260e81c3a4011c9e71f683798adef3f0dd0b
Kedar-Khedkar <70146608+Kedar-Khedkar@users.noreply.github.com>
2023-03-05 23:59:36 +0530

    Add: lazy loading

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,90 +2,114 @@ import "./App.css";
 import "./index.css";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import React from "react";
+import React, { Suspense } from "react";
 import { ForgotPassword } from "./component/Login & Registration/Forget-Password";
 import { ResetPassword } from "./component/Login & Registration/ResetPassword";
-import Dashboard from "./component/Dashboard/Dashboard";
-import MyAccount from "./pages/MyAccount";
-import HeaderAction from "./component/Dashboard/Header";
-import UserTabs from "./pages/UserTabs";
-import AttendanceTabs from "./pages/AttendanceTabs";
-import Subjects from "./pages/Subjects";
-import AuthenticatedRoute from "./Authentication/AuthenticatedRoute";
-import PreventReLogin from "./Authentication/PreventReLogin";
-import AdminRoute from "./Authentication/AdminRoute";
-import FacultyOrAdminRoute from "./Authentication/FacultyOrAdminRoute";
-
+// import Dashboard from "./component/Dashboard/Dashboard";
+// import MyAccount from "./pages/MyAccount";
+// import HeaderAction from "./component/Dashboard/Header";
+// import UserTabs from "./pages/UserTabs";
+// import AttendanceTabs from "./pages/AttendanceTabs";
+// import Subjects from "./pages/Subjects";
+// import AuthenticatedRoute from "./Authentication/AuthenticatedRoute";
+// import PreventReLogin from "./Authentication/PreventReLogin";
+// import AdminRoute from "./Authentication/AdminRoute";
+// import FacultyOrAdminRoute from "./Authentication/FacultyOrAdminRoute";
+import { Center } from "@mantine/core";
 const LoginForm = React.lazy(() =>
   import("./component/Login & Registration/Login")
 );
 const ErrorPage = React.lazy(() => import("./component/Error/ErrorPage"));
+const Dashboard = React.lazy(() => import("./component/Dashboard/Dashboard"));
+const MyAccount = React.lazy(() => import("./pages/MyAccount"));
+const HeaderAction = React.lazy(() => import("./component/Dashboard/Header"));
+const UserTabs = React.lazy(() => import("./pages/UserTabs"));
+const AttendanceTabs = React.lazy(() => import("./pages/AttendanceTabs"));
+const Subjects = React.lazy(() => import("./pages/Subjects"));
+const AuthenticatedRoute = React.lazy(() =>
+  import("./Authentication/AuthenticatedRoute")
+);
+const PreventReLogin = React.lazy(() =>
+  import("./Authentication/PreventReLogin")
+);
+const AdminRoute = React.lazy(() => import("./Authentication/AdminRoute"));
+const FacultyOrAdminRoute = React.lazy(() =>
+  import("./Authentication/FacultyOrAdminRoute")
+);
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <HeaderAction />
-        <Routes>
-          <Route path="*" element={<ErrorPage />} />
-          <Route
-            path="/"
-            element={
-              <PreventReLogin>
-                <LoginForm />
-              </PreventReLogin>
-            }
-          />
-          {<Route path="/forgot-password" element={<ForgotPassword />} />}
-          {<Route path="/reset-password" element={<ResetPassword />} />}
-          <Route
-            path="/dashboard"
-            element={
-              <AuthenticatedRoute>
-                <Dashboard />
-              </AuthenticatedRoute>
-            }
-          />
-          <Route
-            path="/user-mgmt/:tabValue"
-            element={
-              <AuthenticatedRoute>
-                <AdminRoute>
-                  <UserTabs />
-                </AdminRoute>
-              </AuthenticatedRoute>
-            }
-          />
-          <Route
-            path="/attend-mgmt/:tabValue"
-            element={
-              <AuthenticatedRoute>
-                <FacultyOrAdminRoute>
-                  <AttendanceTabs />
-                </FacultyOrAdminRoute>
-              </AuthenticatedRoute>
-            }
-          />
-          <Route
-            path="/subject-mgmt/:tabValue"
-            element={
-              <AuthenticatedRoute>
-                <FacultyOrAdminRoute>
-                  <Subjects />
-                </FacultyOrAdminRoute>
-              </AuthenticatedRoute>
-            }
-          />
-          <Route
-            path="/my-account"
-            element={
-              <AuthenticatedRoute>
-                <MyAccount />
-              </AuthenticatedRoute>
-            }
-          />
-        </Routes>
-      </BrowserRouter>
+      <Suspense
+        fallback={
+          <Center  style={{  height: 700 }}>
+            <span class="loader"></span>
+          </Center>
+        }
+      >
+        <BrowserRouter>
+          <HeaderAction />
+          <Routes>
+            <Route path="*" element={<ErrorPage />} />
+            <Route
+              path="/"
+              element={
+                <PreventReLogin>
+                  <LoginForm />
+                </PreventReLogin>
+              }
+            />
+            {<Route path="/forgot-password" element={<ForgotPassword />} />}
+            {<Route path="/reset-password" element={<ResetPassword />} />}
+            <Route
+              path="/dashboard"
+              element={
+                <AuthenticatedRoute>
+                  <Dashboard />
+                </AuthenticatedRoute>
+              }
+            />
+            <Route
+              path="/user-mgmt/:tabValue"
+              element={
+                <AuthenticatedRoute>
+                  <AdminRoute>
+                    <UserTabs />
+                  </AdminRoute>
+                </AuthenticatedRoute>
+              }
+            />
+            <Route
+              path="/attend-mgmt/:tabValue"
+              element={
+                <AuthenticatedRoute>
+                  <FacultyOrAdminRoute>
+                    <AttendanceTabs />
+                  </FacultyOrAdminRoute>
+                </AuthenticatedRoute>
+              }
+            />
+            <Route
+              path="/subject-mgmt/:tabValue"
+              element={
+                <AuthenticatedRoute>
+                  <FacultyOrAdminRoute>
+                    <Subjects />
+                  </FacultyOrAdminRoute>
+                </AuthenticatedRoute>
+              }
+            />
+            <Route
+              path="/my-account"
+              element={
+                <AuthenticatedRoute>
+                  <MyAccount />
+                </AuthenticatedRoute>
+              }
+            />
+          </Routes>
+        </BrowserRouter>
+      </Suspense>
     </div>
   );
 }
