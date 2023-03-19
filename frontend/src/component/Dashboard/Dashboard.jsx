38d8d299efb86d228c23f92ae1fa78206a31f7a2
Kedar-Khedkar <70146608+Kedar-Khedkar@users.noreply.github.com>
2023-03-19 19:30:31 +0530

    Fix: Background image

--- frontend/src/component/Dashboard/Dashboard.jsx
+++ frontend/src/component/Dashboard/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, SimpleGrid, BackgroundImage } from "@mantine/core";
+import { Container, SimpleGrid, createStyles  } from "@mantine/core";
 import NavigationCard from "./NavigationCard";
 import user from "../../assets/Images/new-entries-animate.svg";
 import attendance from "../../assets/Images/confirmed-attendance-animate.svg";
@@ -9,13 +9,24 @@ import StudentDash from "../Attendance/StudentDash";
 import secureLocalStorage from "react-secure-storage";
 import backgroundImage from "../../assets/Images/ttten1.svg";
 
+const useStyles = createStyles((theme) => ({
+  background: {
+    position: 'relative',
+    backgroundImage:
+        `url(${backgroundImage})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  }
+
+}))
 export default function Dashboard() {
   const { role } = JSON.parse(secureLocalStorage.getItem("user"));
+  const { classes } = useStyles();
   return (
     <>
       {/* <Header/> */}
-      <BackgroundImage src={backgroundImage}>
-        <Container>
+      <div  className={classes.background}>
+        <Container >
           <SimpleGrid
             cols={2}
             breakpoints={[{ maxWidth: 980, cols: 1, spacing: "md" }]}
@@ -51,7 +62,7 @@ export default function Dashboard() {
           </SimpleGrid>
           {role === "student" && <StudentDash></StudentDash>}
         </Container>
-      </BackgroundImage>
+      </div>
     </>
   );
 }
