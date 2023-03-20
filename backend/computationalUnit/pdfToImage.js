59c0ccd78339ab7f43281b9a6c8fa5810a1e526e
Piyush Terkar <terkar.piyush3@gmail.com>
2023-03-20 19:18:38 +0530

    ADD: Image Converter and Image Server

---
+++ backend/computationalUnit/pdfToImage.js
@@ -0,0 +1,23 @@
+const { spawn } = require("child_process");
+
+const convertToImage = (path) => {
+  return new Promise(function (success, error) {
+    const args = ["computationalUnit/pdfToImage.py", path];
+    const pythonProcess = spawn("python", args);
+    let servPath = "";
+    pythonProcess.stdout.on("data", (data) => {
+      servPath += `${data}`;
+    });
+
+    pythonProcess.stderr.on("data", (data) => {
+      console.error(`stderr: ${data}`);
+      error(data);
+    });
+
+    pythonProcess.on("close", (code) => {
+      success(servPath.trim());
+    });
+  });
+};
+
+module.exports = { convertToImage };
---
+++ backend/computationalUnit/pdfToImage.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pdf2image
+import cv2
+import pandas as pd
+from tabula import read_pdf
+from pdf2image import convert_from_path
+import matplotlib.pyplot as plt
+import pytesseract
+from concurrent.futures import ThreadPoolExecutor
+import PyPDF2
+import re
+import sys
+from PIL import Image
+from random import random
+
+pdf_path = sys.argv[1]
+
+
+
+def convert_to_image(page_number):
+    pil_image = pdf2image.convert_from_path(pdf_path,dpi=300,first_page=page_number, last_page=page_number)[0]
+    np_array = np.array(pil_image)
+    bgr_array = cv2.cvtColor(np_array, cv2.COLOR_RGB2BGR)
+    return bgr_array
+
+im = Image.fromarray(convert_to_image(1))
+im_name = f'{int(random()*10000)}.jpeg'
+im.save("./public/temp/" + im_name)
+print(im_name)
