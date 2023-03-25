83dc6cd7f7a3b7ed68559aa5cab82927797a2a14
Piyush Terkar <terkar.piyush3@gmail.com>
2023-03-25 14:35:47 +0530

    ADD: Final Result cleaning and error handling  (MARKS)

--- backend/computationalUnit/extractPdf.js
+++ backend/computationalUnit/extractPdf.js
@@ -1,11 +1,66 @@
 const { spawn } = require("child_process");
+const { Student } = require("../models/student");
+const { Subject } = require("../models/subject");
 
-// const cleanResult = (result, errors) => {
+const cleanResult = async ({ result, errors }) => {
+  let prevSeatno = null;
+  let prevSubject = null;
+  let subCode = null;
+  let uid = null;
+  const finalResult = []; //store the 2D result array to 1D for bulk create
+  for (let i = 0; i < result.length; i++) {
+    for (let j = 0; j < result[i].length; j++) {
+      let currSub = result[i][j];
+      console.log(currSub);
+      if (currSub.SubjectSubCode != prevSubject) {
+        subCode = await Subject.findOne({
+          where: { subCode: currSub.SubjectSubCode },
+          attributes: ["subCode"],
+        });
+        if (subCode) {
+          prevSubject = subCode.subCode;
+        }
+        console.log(prevSubject);
+      }
+      if (currSub.seatno != prevSeatno) {
+        console.log("Hit");
+        uid = await Student.findOne({
+          where: { examseatno: currSub.seatno },
+          attributes: [["UserId", "userId"]],
+        });
+      }
+      if (subCode && uid) {
+        delete currSub.seatno;
+        currSub.StudentUserId = uid.userId;
+        console.log(currSub);
+        finalResult.push(currSub);
+      } else {
+        if (!subCode) {
+          errors.push({
+            value: currSub,
+            error: `No subject: ${currSub.SubjectSubCode}`,
+          });
+        } else {
+          errors.push({
+            value: currSub,
+            error: `No student with seat no: ${currSub.seatno}`,
+          });
+        }
+      }
+    }
+  }
+  return { result: finalResult, errors: errors };
+};
 
-// };
+const getresult = async () => {
+  let result = await cleanResult({ result: test, errors: [] });
+  console.log(result.result, result.errors);
+};
+
+// getresult();
 
 const spawnProcess = (coords, seatNos, pages, path) => {
-  return new Promise(function (success, error) {
+  return new Promise(async function (success, error) {
     // Send SSE headers
     //res.writeHead(200, {
     //   "Content-Type": "text/event-stream",
@@ -51,10 +106,9 @@ const spawnProcess = (coords, seatNos, pages, path) => {
       });
       console.log(`child process exited with code ${code}`);
       //res.write(`data: ${JSON.stringify({ done: true })}\n\n`);
-      // {result, errors} = cleanResult(result, errors);
       success({ result: result, errors: errors });
     });
   });
 };
 
-module.exports = { spawnProcess };
+module.exports = { spawnProcess, cleanResult };
