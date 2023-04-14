0c4094b8d38686fbf16044e505643ee022ef3ee7
Kedar-Khedkar <70146608+Kedar-Khedkar@users.noreply.github.com>
2023-04-14 15:04:33 +0530

    Add: axios interceptor to clear localstorage

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,13 +5,29 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { NotificationsProvider } from "@mantine/notifications";
 import { ModalsProvider } from "@mantine/modals";
-// import "bootstrap/dist/css/bootstrap.min.css";
+import secureLocalStorage from "react-secure-storage";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 // import { AuthProvider } from "./component/context/UserContext";
 // import { NotifyProvider } from "./component/context/ToastContext";
 // import * as serviceWorker from './serviceWorker';
-
+import axios from 'axios';
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+
+/* This code is adding a response interceptor to the Axios library. The interceptor is a function that
+gets called for every response that Axios receives. */
+axios.interceptors.response.use(
+  response => response, // pass through non-error responses
+  error => {
+    // check if the error is due to a 401 status code
+    if (error.response && error.response.status === 401) {
+      // delete the local storage
+      secureLocalStorage.clear();
+      console.log("local storage cleared")
+    }
+    return Promise.reject(error);
+  }
+);
 
 root.render(
   <React.StrictMode>
