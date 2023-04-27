ad1ab5f354c49eb6b12372188c55ef36311bc6fb
Piyush Terkar <terkar.piyush3@gmail.com>
2023-04-27 21:42:18 +0530

    CONTAINERIZE: backend and add start scripts

--- backend/index.js
+++ backend/index.js
@@ -44,12 +44,14 @@ app.use(
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "public")));
 
-try {
-  sequelize.authenticate();
-  console.log("Connection has been established successfully.");
-} catch (error) {
-  console.error("Unable to connect to the database:", error);
-}
+sequelize
+  .authenticate()
+  .then((res) => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
 
 const store = new sessionStore({
   db: sequelize,
--- backend/utils/database.js
+++ backend/utils/database.js
@@ -2,7 +2,7 @@ const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize("ddms", "app", "password", {
   logging: false,
-  host: "localhost",
+  host: "db",
   dialect: "mysql",
 });
 
