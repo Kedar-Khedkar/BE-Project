2d09daf83a1801579912b5715a4c597997e29de9
Piyush Terkar <terkar.piyush3@gmail.com>
2023-04-28 13:08:29 +0530

    DEPLOYMENT: configure prod env variables

--- backend/utils/database.js
+++ backend/utils/database.js
@@ -1,9 +1,32 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("ddms", "app", "password", {
-  logging: false,
-  host: "db",
-  dialect: "mysql",
-});
+let config = {
+  db_host: "localhost",
+  db_port: 3306,
+  db_user: "app",
+  db_password: "password",
+  db_name: "ddms",
+};
+
+if (process.env == "production") {
+  console.log("PRODUCTION ENVIRONMENT VARIABLES SET");
+  config.db_host = process.env.DB_HOST;
+  config.db_port = process.env.DB_PORT;
+  config.db_user = process.env.DB_USER;
+  config.db_password = process.env.DB_PASSWORD;
+  config.db_name = process.env.DB_NAME;
+}
+
+const sequelize = new Sequelize(
+  config.db_name,
+  config.db_user,
+  config.db_password,
+  {
+    logging: false,
+    host: config.db_host,
+    port: config.db_port,
+    dialect: "mysql",
+  }
+);
 
 module.exports = { Sequelize, sequelize };
--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,8 +1,13 @@
-import axios from 'axios';
+import axios from "axios";
 
-const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000',
-  withCredentials:true
-});
+let config = {
+  baseURL: "http://localhost:5000",
+  withCredentials: true,
+};
+if (process.env === "production") {
+  console.log("PRODUCTION ENVIRONMENT VARIABLES SET");
+  config.baseURL = `http://${process.env.BACKEND_HOST}:${process.env.BACKEND_PORT}`;
+}
+const axiosInstance = axios.create(config);
 
 export default axiosInstance;
